7b4c35a399246f20c8cfcbf86ed7dbd8617cae28
Rijnard van Tonder <rvantonder@gmail.com>
2020-01-06 13:42:47 -0700

    hardcode threshold to 100 for matching files per repo

--- cmd/frontend/graphqlbackend/textsearch.go
+++ cmd/frontend/graphqlbackend/textsearch.go
@@ -613,7 +613,12 @@ func searchFilesInRepos(ctx context.Context, args *search.TextParameters) (res [
 					patternCopy := *args.PatternInfo
 					args.PatternInfo = &patternCopy
 					includePatternsCopy := []string{}
-					args.PatternInfo.IncludePatterns = append(includePatternsCopy, v...)
+					threshold := 100 // if there are more matching files than this threshold, only search the first 100.
+					if len(v) > threshold {
+						args.PatternInfo.IncludePatterns = append(includePatternsCopy, v[:100]...)
+					} else {
+						args.PatternInfo.IncludePatterns = append(includePatternsCopy, v...)
+					}
 				}
 			}
 
